e9fb97e3fb6b097e01c2b447232b29aca4a30296
Kristina <kristina@Kristinas-MacBook-Pro.local>
2023-06-28 21:35:37 -0500

    edited schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,23 +5,24 @@ USE calendar_db;
 
 CREATE TABLE months (
     id INT PRIMARY KEY,
-    db_date DATE NOT NULL,
-    month INTEGER NOT NULL,
-    week INTEGER NOT NULL,
-    day INTEGER NOT NULL,
-    month_name VARCHAR(10) NOT NULL,
-    day_name VARCHAR(10) NOT NULL
+    db_date DATE,
+    month INTEGER,
+    week INTEGER,
+    day INTEGER,
+    month_name VARCHAR(10),
+    day_name VARCHAR(10)
 );
 
 CREATE TABLE weeks (
     weeks_id INT AUTO_INCREMENT PRIMARY KEY,
     weeks VARCHAR(30),
     dates INT,
-    FOREIGN KEY (weeks_id) REFERENCES months(id) ON DELETE SET NULL
+    FOREIGN KEY (weeks_id) REFERENCES months(id)
 );
 
 CREATE TABLE days(
     days_id INT AUTO_INCREMENT PRIMARY KEY,
     days VARCHAR(30),
     dates INT,
-    FOREIGN KEY (dates_id) REFERENCES weeks(weeks_id) ON DELETE SET NULL
+    FOREIGN KEY (days_id) REFERENCES weeks(weeks_id)
+);
