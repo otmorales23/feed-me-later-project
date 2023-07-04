32d81dd66ff8148e9d9eb52073916dce4c791dac
Kristina <kristina@Kristinas-MacBook-Pro.local>
2023-07-03 18:20:17 -0700

    inserted seed

--- db/schema.sql
+++ db/schema.sql
@@ -3,40 +3,18 @@ CREATE DATABASE calendar_db;
 
 USE calendar_db;
 
-CREATE TABLE months (
-    id INT PRIMARY KEY,
-    db_date DATE,
-    month INTEGER,
-    week INTEGER,
-    day INTEGER,
-    month_name VARCHAR(10),
-    day_name VARCHAR(10)
-);
-
-CREATE TABLE weeks (
-    weeks_id INT AUTO_INCREMENT PRIMARY KEY,
-    weeks VARCHAR(30),
-    dates INT,
-    FOREIGN KEY (weeks_id) REFERENCES months(id)
-);
-
-CREATE TABLE days(
-    days_id INT AUTO_INCREMENT PRIMARY KEY,
-    days VARCHAR(30),
-    dates INT,
-    FOREIGN KEY (days_id) REFERENCES weeks(weeks_id)
-);
-
 CREATE TABLE business(
-    business_id VARCHAR,
-    business_name VARCHAR,
-    category VARCHAR,
-    location_address VARCHAR,
-    location_city VARCHAR,
-    location_state VARCHAR,
-    location_country VARCHAR,
-    hour_start INT,
-    hour_end INT,
-    rating INT,
-    business_url VARCHAR
+    id VARCHAR,
+    locationName VARCHAR,
+    locationAddress VARCHAR,
+    locationCity VARCHAR,
+    locationState VARCHAR
 )
+
+CREATE TABLE user(
+    id VARCHAR,
+    username VARCHAR
+)
+
+INSERT INTO business (id, locationName, locationAddress, locationCity, locationState)
+  VALUES (1, "McDonalds", "123 St.", "San Jose", "CA");
