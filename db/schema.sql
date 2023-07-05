329398ce3da8eaaf4b02847290deddf33f8a21dd
Kristina <kristina@Kristinas-MacBook-Pro.local>
2023-07-05 12:37:49 -0700

    troubleshoot tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,21 @@ CREATE DATABASE calendar_db;
 USE calendar_db;
 
 CREATE TABLE business(
-    id VARCHAR,
-    locationName VARCHAR,
-    locationAddress VARCHAR,
-    locationCity VARCHAR,
-    locationState VARCHAR
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    locationName VARCHAR(30),
+    locationAddress VARCHAR(30),
+    locationCity TEXT,
+    locationState TEXT
+);
 
 CREATE TABLE user(
-    id VARCHAR,
-    username VARCHAR
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    userName VARCHAR(30),
+    userPassword VARCHAR(30)
+);
 
 INSERT INTO business (id, locationName, locationAddress, locationCity, locationState)
-  VALUES (1, "McDonalds", "123 St.", "San Jose", "CA");
+    VALUES (1, "McDonalds", "123 St.", "San Jose", "CA");
+
+INSERT INTO user(id, userName, userPassword)
+    VALUES (1, "bootcampuser", "lavender");
